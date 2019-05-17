a8d904fe2122d629c7a50227395ba5535d21c874
Rocky Green <rockjock51@gmail.com>
2019-05-16 22:42:00 -0500

    Day 46: Making test cases better for ExpensesSummary

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -2,21 +2,21 @@ import React from "react";
 import { shallow } from "enzyme";
 import { ExpensesSummary } from "../../components/ExpensesSummary";
 import expenses from "../fixtures/expenses";
-
-let expenseTotal;
-
-beforeEach((expenseTotal = jest.fn()));
+import selectExpensesTotal from "../../selectors/expenses-total";
 
 test("should render correctly", () => {
   const wrapper = shallow(
-    <ExpensesSummary expenses={expenses} expenseTotal={expenseTotal} />
+    <ExpensesSummary expenses={expenses} expenseTotal={selectExpensesTotal} />
   );
   expect(wrapper).toMatchSnapshot();
 });
 
 test("should render 1 expense correctly", () => {
   const wrapper = shallow(
-    <ExpensesSummary expenses={expenses[1]} expenseTotal={expenseTotal} />
+    <ExpensesSummary
+      expenses={[expenses[1]]}
+      expenseTotal={selectExpensesTotal}
+    />
   );
   expect(wrapper).toMatchSnapshot();
 });
